e583e2bbfa49e28ed75705395b37bfbeeae742fe
Nirmohi <nirmohi0605@gmail.com>
2016-07-12 17:05:57 -0500

    set up routes

---
+++ app/containers/Main.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Main = React.createClass({
+	render: function(){
+		return (
+		<div>
+			<h1>This is a header</h1>
+		</div>	
+
+		)
+	}
+})
+
+
+module.exports = Main;
